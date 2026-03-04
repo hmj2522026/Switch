48379528e596da8667c8a96284cfe8665109b6e9
hmj2522026 <hmj2522026@stu.o-hara.ac.jp>
2026-03-04 09:34:56 +0900

    GameMain追加

--- Switch/GameConfig.h
+++ Switch/GameConfig.h
@@ -1,1 +1,10 @@
 #pragma once
+
+namespace GameConfig
+{
+	static const char* Title = "Switch";	// タイトル
+	static constexpr bool AlwaysRunFlag = true;		// 非アクティブ時にも動作するか
+	static constexpr bool WindowMode = true;		// Windowモードとフルスクリーンどっちで起動するか
+	static constexpr int ColorBit = 32;				// カラービット数
+	static constexpr int FPS = 60;
+}
---
+++ Switch/GameMain.cpp
@@ -0,0 +1,86 @@
+#include "DxLib.h"
+#include "GameMain.h"
+#include "GameConfig.h"
+#include "Screen.h"
+#include "SceneManager.h"
+#include "SceneGame.h"
+#include "Time.h"
+#include "Keyboard.h"
+
+GameMain::~GameMain()
+{
+	// 自作スクリーンの破棄
+	DeleteGraph(m_screen);
+
+	DxLib_End();
+}
+
+// メインループ
+void GameMain::Run()
+{
+	// DxLib前処理
+	ChangeWindowMode(GameConfig::WindowMode);	// ウィンドウモード
+	SetMainWindowText(GameConfig::Title);		// ウィンドウタイトル
+	SetGraphMode(Screen::Width, Screen::Height, GameConfig::ColorBit);	// ウィンドウサイズ
+	SetAlwaysRunFlag(GameConfig::AlwaysRunFlag);	// 非アクティブ時の動作
+	SetWaitVSyncFlag(false);
+
+	if (DxLib_Init() == -1)
+	{
+		throw - 1;
+	}
+
+	// シーン起動
+	SceneManager::GetInstance()->Setup(new SceneGame());
+
+	// スクリーン作成
+	m_screen = MakeScreen(Screen::Width, Screen::Height);
+
+	LONGLONG frameStartTime;
+
+	while (true)
+	{
+		// フレームの開始時刻取得（単位　マイクロ秒）
+		frameStartTime = GetNowHiPerformanceCount();
+
+		if (ProcessMessage())
+		{
+			break;
+		}
+
+		if (CheckHitKey(KEY_INPUT_ESCAPE))
+		{
+			break;
+		}
+
+		// 時間の更新
+		Time::GetInstance()->Update();
+
+		// キー入力状態の更新
+		Keyboard::Update();
+
+		// シーンの更新
+		SceneManager::GetInstance()->Update();
+
+		// 自作スクリーンに描画
+		SetDrawScreen(m_screen);
+		ClearDrawScreen();
+
+		// シーンの描画
+		SceneManager::GetInstance()->Draw();
+
+		// 自作スクリーンを裏画面に描画
+		SetDrawScreen(DX_SCREEN_BACK);
+		ClearDrawScreen();
+		DrawGraph(0, 0, m_screen, false);
+
+		// 裏画面と表画面をひっくり返す
+		ScreenFlip();
+
+		// フレーム内の経過時間が指定したFPSに到達するまで待つ
+		while (GetNowHiPerformanceCount() - frameStartTime < 1000000 / GameConfig::FPS);
+	}
+	
+	// シーンの破棄
+	SceneManager::GetInstance()->Dispose();
+}
